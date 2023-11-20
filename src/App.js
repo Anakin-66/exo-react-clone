e81566efc12430e3da5d0ff5b793051f592a1cc8
Frangeul Nathan <seekerz17@outlook.fr>
2023-11-20 11:23:54 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+// J'importe les variable du react router 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./page/HomePage";
+import ProductsPage from "./page/ProductsPage";
+import ProductPage from "./page/ProductPage";
+
+function App() {
+  return (
+    // Je déclare le browserrouter, avec à l'intérieur mes routes et leur URL associés (ex : mon /products sera lié à ProductsPage)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products/:id" element={<ProductPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
