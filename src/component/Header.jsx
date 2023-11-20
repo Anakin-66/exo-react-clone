081c6ec60c9edfa949cdf70c9b8f07e0f37d7ab8
Frangeul Nathan <seekerz17@outlook.fr>
2023-11-20 14:05:02 +0100

    added props 2

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -7,7 +7,7 @@ function Header({pageTitle}) {
         src="https://www.zarla.com/images/zarla-lectrodomus-1x1-2400x2400-20220705-x44pr4y4bq86kgcbw36t.png?crop=1:1,smart&width=250&dpr=2"
         alt="electro menager"
       />
-
+      {/* Je met mon paramètre dans ma balise p, je pourrais ensuite changer le texte à ma guise en l'appelant dans mes autres pages */}
       <p> {pageTitle} </p>
 
       <nav>
